155d66dd86083e17b93c9a5c0534adfee18a121e
jmlb23 <rmmenosrf@riseup.net>
2025-12-17 12:31:12 +0100

    added description and tittle

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from features.users import routes as userRoutes
 from features.posts import routes as postRoutes
 
 
-app = FastAPI()
+app = FastAPI(description="a blog api", title="blog")
 
 app.include_router(userRoutes.userRouter)
 app.include_router(postRoutes.postRouter)
