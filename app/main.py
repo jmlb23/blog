4e34e49c34538cb34ce135e0f12f9ec1401c9c67
jmlb23 <rmmenosrf@riseup.net>
2026-01-19 10:38:23 +0100

    add host

--- app/main.py
+++ app/main.py
@@ -5,7 +5,13 @@ from features.comments import routes as commentsRoutes
 from dotenv import load_dotenv
 
 load_dotenv()
-app = FastAPI(description="a blog api", title="blog")
+app = FastAPI(
+    description="a blog api",
+    title="blog",
+    servers=[
+        {"url": "127.0.0.1",
+         "description": "Local environment"},
+    ])
 
 app.include_router(userRoutes.userRouter)
 app.include_router(postRoutes.postRouter)
